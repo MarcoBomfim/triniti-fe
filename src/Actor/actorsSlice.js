6f8a3a94863def13880aaa3b6c55670193a8ab7a
Marco Bomfim <marcoabomfim3@gmail.com>
2023-05-19 20:05:54 +1000

    chore: create actors and movies slices

---
+++ src/Actor/actorsSlice.js
@@ -0,0 +1,82 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3001';
+
+export const fetchActorsRequest = createAsyncThunk('actors/fetchActors', async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/actors`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching actors:', error);
+    throw error;
+  }
+});
+
+export const addActorRequest = createAsyncThunk('actors/addActor', async (actorData) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/actors`, actorData);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating actor:', error);
+    throw error;
+  }
+});
+
+export const addMovieToActorRequest = createAsyncThunk(
+  'actors/addMovieToActor',
+  async ({ actorId, movieId }) => {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/actors/${actorId}/movies`, { movieId });
+      return response.data;
+    } catch (error) {
+      console.error('Error adding movie to actor:', error);
+      throw error;
+    }
+  }
+);
+
+export const actorsSlice = createSlice({
+  name: "actors",
+  initialState: [],
+  reducers: {
+    setActors: (state, action) => {
+      return action.payload
+    },
+    addActor: (state, action) => {
+      state.push(action.payload);
+    },
+    addMovieToActor: (state, action) => {
+      const { actorId, movieId } = action.payload;
+      
+      const actor = state.find((actor) => actor.id === actorId);
+      
+      if (actor) {
+        actor.movieIds.push(movieId);
+        state = { ...state, actor }
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchActorsRequest.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(addActorRequest.fulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      .addCase(addMovieToActorRequest.fulfilled, (state, action) => {
+        const { actorId, movieId } = action.payload;
+        const actor = state.find((actor) => actor.id === actorId);
+        if (actor) {
+          actor.movieIds.push(movieId);
+        }
+      });
+  },
+});
+
+export const { setActors, addActor, addMovieToActor } = actorsSlice.actions;
+
+export default actorsSlice.reducer;
+
+
---
+++ src/Movie/moviesSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { nanoid } from "nanoid";
+
+const API_BASE_URL = 'http://localhost:3001';
+
+export const fetchMoviesRequest = createAsyncThunk('movies/fetchMovies', async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/movies`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    throw error;
+  }
+});
+
+export const addMovieRequest = createAsyncThunk('movies/createMovie', async (payload) => {
+  try {
+    const { actorId, movie } = payload;
+    console.log(actorId, payload)
+    const response = await axios.post(`${API_BASE_URL}/actors/${actorId}/movies`, movie);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating movie:', error);
+    throw error;
+  }
+});
+
+export const moviesSlice = createSlice({
+  name: "movies",
+  initialState: [
+  ],
+  reducers: {
+    setMovies: (state, action) => {
+      return action.payload
+    },
+    addMovie: (state, action) => {
+      const { name, releaseDate } = action.payload;
+      state.push({ id: nanoid(), name, releaseDate });
+    },
+    removeMovie: (state, action) => {
+      const movieId = action.payload;
+      return state.filter((movie) => movie.id !== movieId);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMoviesRequest.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(addMovieRequest.fulfilled, (state, action) => {
+        state.push(action.payload);
+      });
+  },
+});
+
+
+export const { setMovies, addMovie, removeMovie } = moviesSlice.actions;
+export default moviesSlice.reducer;
