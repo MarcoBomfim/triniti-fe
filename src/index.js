46100ffdd186f6018dbcdb6bb618139334f74c7c
Marco Bomfim <marcoabomfim3@gmail.com>
2023-05-19 20:06:39 +1000

    chore: update styles to suport material ui, and add routes and store file to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import ActorPage from "./Actor/ActorPage";
+import CreateActorPage from "./Actor/CreateActorPage";
+import CreateMoviePage from "./Movie/CreateMoviePage";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+import store from "./store";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/actor/:id",
+    element: <ActorPage />,
+  },
+  {
+    path: "/new-actor",
+    element: <CreateActorPage />,
+  },
+  {
+    path: "/new-movie/:actorId",
+    element: <CreateMoviePage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+
+      <RouterProvider router={router} />
+
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
